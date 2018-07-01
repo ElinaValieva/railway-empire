9c8e54b9951a2196ef952ca79f4e9f6ce03095d9
ElinaValieva <veaufa@mail.ru>
2018-07-01 12:14:29 +0300

    fix authorized person in edit profile

--- src/main/java/com/elina/railwayApp/controller/UserController.java
+++ src/main/java/com/elina/railwayApp/controller/UserController.java
@@ -10,6 +10,8 @@ import com.elina.railwayApp.service.UserService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
