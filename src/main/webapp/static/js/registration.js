7b825e4acdce5ae3314f06bcff69eb01ec87359c
ElinaValieva <veaufa@mail.ru>
2018-07-05 01:59:20 +0300

    feat(with error 500): ajax jquery requests

--- src/main/webapp/static/js/registration.js
+++ src/main/webapp/static/js/registration.js
@@ -1,24 +1,31 @@
 $(function () {
-    $('#registrationBtn').click(function () {
-        alert('aaa');
-        var user = {};
-        user['firstName'] = $('#firstName').val();
-        user['lastName'] = $('#lastName').val();
-        user['login'] = $('#login').val();
-        user['password'] = $('#password').val();
-        user['birthDay'] = '';
-        user['sex'] = '';
+    $('#registrationBtn').click(function (event) {
+        event.preventDefault();
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
+        alert(header);
+        alert(token);
+        var user = {
+            firstName: $('#firstName').val(),
+            lastName: $('#lastName').val(),
+            login: $('#login').val(),
+            password: $('#password').val()
+        }
         $.ajax({
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            },
             url: "/registration",
             data: JSON.stringify(user),
             method: "POST",
             contentType: "application/json",
             dataType: 'json'
         }).done(function () {
-            alert('success');
             window.location.href = "/login"
-        }).fail(function () {
-            alert('error');
-        })
+        }).fail(function (qXHR, textStatus, errorThrown) {
+            console.log('request: ', JSON.stringify(qXHR));
+            console.log('status text: ', textStatus);
+            console.log('thrown error: ', JSON.stringify(errorThrown));
+        });
     })
 });
