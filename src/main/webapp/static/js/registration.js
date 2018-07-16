d01a6f052edaed4830f722f4ee57df2014ca301a
ElinaValieva <veaufa@mail.ru>
2018-07-16 13:23:28 +0300

    100! fix(registration page): validate fields for containing letters and fields length

--- src/main/webapp/static/js/registration.js
+++ src/main/webapp/static/js/registration.js
@@ -1,16 +1,47 @@
 $(function () {
+
     $('#registrationBtn').click(function (event) {
         event.preventDefault();
-        var userDTO = {
-            firstName: $('#firstName').val(),
-            lastName: $('#lastName').val(),
-            login: $('#login').val(),
-            password: $('#password').val(),
-            sex: '',
-            birthDay: ''
-        };
-        alert(JSON.stringify(userDTO));
-        var urlSearching = "/registration";
-        postRequest(userDTO, urlSearching, "You have been registered!");
-    })
+        if (checkFields()) {
+            var userDTO = {
+                firstName: $('#firstName').val(),
+                lastName: $('#lastName').val(),
+                login: $('#login').val(),
+                password: $('#password').val(),
+                sex: '',
+                birthDay: ''
+            };
+            var urlSearching = "/registration";
+            postRequest(userDTO, urlSearching, "You have been registered!");
+        }
+    });
+
+    /**
+     * 1. Fields firstName, lastName don't contain numbers
+     * 2. Length for field password >6
+     */
+    var checkFields = function () {
+
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
+        var pass = $('#password').val();
+
+        if (!/^[a-zA-Z]+$/.test(firstName) || !/^[a-zA-Z]+$/.test(lastName)) {
+            swal({
+                title: 'Oops...',
+                text: 'Fields for name and last name must contain only letters!',
+                type: 'error'
+            });
+            return false;
+        }
+        else if (pass.length < 6) {
+            swal({
+                title: 'Oops...',
+                text: 'Field for password should be longer than yours!',
+                type: 'error'
+            });
+            return false;
+        }
+        else return true;
+    }
 });
